d9920f10e78e0a7f4e893a1e3e48ac4bfe8125b9
Alejandro Herrero <joseherrero85@icloud.com>
2022-03-14 12:59:18 -0300

    Initial Commit

---
+++ WeatherSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherSearcher
+//
+//  Created by Jose Alejandro Herrero on 3/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
